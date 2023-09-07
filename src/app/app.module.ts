770c3f5f634fe68e9e108027b5c5342aa0fe3b42
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-09-07 20:03:21 -0300

    iniciando con los estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 
 import { TasksComponent } from './components/tasks/tasks.component';
 import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, TasksComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MatListModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
